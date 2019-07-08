f84efb7fb96ad86c9167fd35bea0700cbf08b0ab
amit <amit.kadivar1@gmail.com>
2019-07-09 03:19:15 +0530

    change in logic

--- mutualfund/views.py
+++ mutualfund/views.py
@@ -14,58 +14,55 @@ api_key = "x19ec-fDEy26ozp7xHFz"
 
 
 def inputform(request):
-    datadict=[]
+    datadict = []
     data = requests.get('http://portal.amfiindia.com/spages/NAVAll.txt')
     if (data.status_code) == 200:
         mutualfund = data.content
         mutualfund = str(mutualfund).split('\\r\\n')
         for i in mutualfund:
-            name=i.split(';')
-            if(len(name)>5):
-                name=i.split(';')[-3]
-                code=i.split(';')[0]
-                datadict.append({'code':code,'name':name})                
-                
-    return render(request,'index.html',context={'datadict':datadict})
+            name = i.split(';')
+            code = i.split(';')[0]
+            if(len(name) > 5 and code.isnumeric()):
+                name = i.split(';')[-3]
+                datadict.append({'code': code, 'name': name})
+    return render(request, 'index.html', context={'datadict': datadict})
+
 
 def getmutualfund(request):
     try:
         income = request.POST['amount']
-        year = request.POST['year'] 
-        plan=request.POST['plan']  
+        year = request.POST['year']
+        plan = request.POST['plan']
         try:
             dictname = []
             previousYear = datetime.now() - relativedelta(years=int(year))
-            previousYear=str(previousYear).split()[0]
+            previousYear = str(previousYear).split()[0]
             print(previousYear)
-            api=requests.get("https://www.quandl.com/api/v3/datasets/AMFI/{}.json?api_key={}".format(plan,api_key))
-            if api.status_code==200:
-                api=api.json()
-                dataset=api['dataset']
-                apidata=dataset['data']
-                # print(apidata,'------------------------------')
-                nav=int(apidata[0][1])
-                
+            api = requests.get(
+                "https://www.quandl.com/api/v3/datasets/AMFI/{}.json?api_key={}".format(plan, api_key))
+            if api.status_code == 200:
+                api = api.json()
+                dataset = api['dataset']
+                apidata = dataset['data']
+                nav = int(apidata[0][1])
                 for i in apidata:
-                    if(i[0]==previousYear):
-                        initalnav=int(i[1])
-                        ar=(nav-initalnav)/initalnav*100
-                        valueamount=int(income)+((int(income)*float(ar))/100)
-                        print(valueamount,ar)
-                        dictname.append({'Code': dataset['dataset_code'], 'Name': dataset['name'],'Nav': nav, 'ReturnFund': str(round(ar,2)),'ReturnFundValue':str(round(valueamount,2)),'InitialData':str(previousYear), 'Date': str(dataset['end_date']),'amount':income,})
-                        break    
-                return render(request,'detail.html',context={'dictname':dictname,'nav':nav})
+                    if(i[0] == previousYear):
+                        initalnav = int(i[1])
+                        ar = (nav-initalnav)/initalnav*100
+                        valueamount = int(income)+((int(income)*float(ar))/100)
+                        print(valueamount, ar)
+                        dictname.append({'Code': dataset['dataset_code'], 'Name': dataset['name'], 'Nav': nav, 'ReturnFund': str(round(ar, 2)), 'ReturnFundValue': str(
+                            round(valueamount, 2)), 'InitialData': str(previousYear), 'Date': str(dataset['end_date']), 'amount': income, })
+                        break
+                return render(request, 'detail.html', context={'dictname': dictname, 'nav': nav})
             else:
                 return HttpResponse('Oops!!! Status Code', api.status_code)
         except Exception as e:
             print(e)
-            return HttpResponse("Exception ",e)
-    # print(allmutalfund)yearyear
-      
-            # return HttpResponse(json.dumps(dictname))
+            return HttpResponse("Exception ", e)
     except Exception as e:
-        return HttpResponse("Exception ",e, api.status_code)
-    
+        return HttpResponse("Exception ", e, api.status_code)
+
 
 # using this you can get only one value this api is optional
 def mutualfunddetail(request, schemacode):
