1221ad1377466d89066c2b189acb74ab4786e121
amit <amit.kadivar1@gmail.com>
2019-07-09 03:14:58 +0530

    change in logic

--- mutualfund/views.py
+++ mutualfund/views.py
@@ -1,40 +1,70 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 import requests
+
 import json
 headers = {
     "X-RapidAPI-Host": "dmoin-mutual-fund-nav-bse-india-v1.p.rapidapi.com",
     "X-RapidAPI-Key": "c7c2dfe082msh466ae3982981b5dp195f31jsne3bdbc4aeb94"
 }
+api_key = "x19ec-fDEy26ozp7xHFz"
+
 
 def inputform(request):
-    return render(request,'index.html',{})
+    datadict=[]
+    data = requests.get('http://portal.amfiindia.com/spages/NAVAll.txt')
+    if (data.status_code) == 200:
+        mutualfund = data.content
+        mutualfund = str(mutualfund).split('\\r\\n')
+        for i in mutualfund:
+            name=i.split(';')
+            if(len(name)>5):
+                name=i.split(';')[-3]
+                code=i.split(';')[0]
+                datadict.append({'code':code,'name':name})                
+                
+    return render(request,'index.html',context={'datadict':datadict})
 
 def getmutualfund(request):
     try:
         income = request.POST['amount']
         year = request.POST['year'] 
-        print(income,year)
-        allmutalfund = requests.get('https://dmoin-mutual-fund-nav-bse-india-v1.p.rapidapi.com/GetAllFundsData', headers=headers)
-    # print(allmutalfund)yearyear
-        if allmutalfund.status_code == 200:
+        plan=request.POST['plan']  
+        try:
             dictname = []
-            data = allmutalfund.json()
-            for i in data:
-                nav = i['NetAssetValue']
-                if(nav != 'N.A.'):
-                    returnfund = int(year)*int(income)*float(nav)
-                    print(returnfund)
-                else:
-                    returnfund = 0
-                dictname.append({'Code': i['SchemeCode'], 'Name': i['SchemeName'],'Nav': nav, 'ReturnFund': str(returnfund), 'Date': i['NAVDate']})
-            return render(request,'detail.html',context={'dictname':dictname})
+            previousYear = datetime.now() - relativedelta(years=int(year))
+            previousYear=str(previousYear).split()[0]
+            print(previousYear)
+            api=requests.get("https://www.quandl.com/api/v3/datasets/AMFI/{}.json?api_key={}".format(plan,api_key))
+            if api.status_code==200:
+                api=api.json()
+                dataset=api['dataset']
+                apidata=dataset['data']
+                # print(apidata,'------------------------------')
+                nav=int(apidata[0][1])
+                
+                for i in apidata:
+                    if(i[0]==previousYear):
+                        initalnav=int(i[1])
+                        ar=(nav-initalnav)/initalnav*100
+                        valueamount=int(income)+((int(income)*float(ar))/100)
+                        print(valueamount,ar)
+                        dictname.append({'Code': dataset['dataset_code'], 'Name': dataset['name'],'Nav': nav, 'ReturnFund': str(round(ar,2)),'ReturnFundValue':str(round(valueamount,2)),'InitialData':str(previousYear), 'Date': str(dataset['end_date']),'amount':income,})
+                        break    
+                return render(request,'detail.html',context={'dictname':dictname,'nav':nav})
+            else:
+                return HttpResponse('Oops!!! Status Code', api.status_code)
+        except Exception as e:
+            print(e)
+            return HttpResponse("Exception ",e)
+    # print(allmutalfund)yearyear
+      
             # return HttpResponse(json.dumps(dictname))
-        else:
-            return HttpResponse('Oops!!! Status Code', allmutalfund.status_code)
     except Exception as e:
-        return HttpResponse("Exception ",e, allmutalfund.status_code)
+        return HttpResponse("Exception ",e, api.status_code)
     
 
 # using this you can get only one value this api is optional
